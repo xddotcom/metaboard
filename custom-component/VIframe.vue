b88fdd686e2bb134841f4ba31dd8e9a8335e8884
liran319 <liran319@gmail.com>
2021-08-21 18:12:16 +0800

    Update VIframe.vue

--- custom-component/VIframe.vue
+++ custom-component/VIframe.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="v-iframe">
     <iframe :src="propValue" frameborder="0" allowfullscreen></iframe>
-    <!-- <div v-if="!allowClick" class="iframe-mask" @dblclick="setClickAvailable"></div> -->
+    <div v-if="!allowClick" class="iframe-mask" @dblclick="setClickAvailable"></div>
   </div>
 </template>
 
@@ -24,8 +24,8 @@ export default {
   },
   methods: {
     setClickAvailable() {
-      if (this.element.isLock) return
-      this.allowClick = true
+      // if (this.element.isLock) return
+      // this.allowClick = true
     },
     handleBlur() {
       console.log('blur div!!!')
@@ -49,18 +49,18 @@ iframe {
   right: 0;
   bottom: 0;
   left: 0;
-  background-color: rgba(0, 0, 0, 0.4);
-  &::after {
-    content: '双击以开启iframe交互';
-    position: absolute;
-    top: 50%;
-    left: 0;
-    right: 0;
-    padding: 10px;
-    transform: translateY(-50%);
-    font-size: 14px;
-    color: #ffffff;
-    text-align: center;
-  }
+  background-color: rgba(0, 0, 0, 0.1);
+  // &::after {
+  //   content: '双击以开启iframe交互';
+  //   position: absolute;
+  //   top: 50%;
+  //   left: 0;
+  //   right: 0;
+  //   padding: 10px;
+  //   transform: translateY(-50%);
+  //   font-size: 14px;
+  //   color: #ffffff;
+  //   text-align: center;
+  // }
 }
 </style>
